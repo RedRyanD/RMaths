31e7f0983fa42e8c6d24f2278ac2f73df659e07d
RedRyanD <53630616+RedRyanD@users.noreply.github.com>
2021-07-28 12:57:06 +1000

    Initial commit

---
+++ inc/inc.h
@@ -0,0 +1,6 @@
+#ifndef INC_H
+#define INC_H
+
+
+
+#endif /* INC_H */
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include<limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+#include <functional>
+
+int main()
+{
+    
+    return 0;
+}
